e70b1929089f7669d0091652bcb9b2553abb87eb
hoando <hoando.dev@gmail.com>
2024-12-08 10:12:34 +0700

    feat: update read_request function

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
 use async_trait::async_trait;
 
-use futures::{AsyncRead, StreamExt};
+use futures::{future::poll_fn, StreamExt};
 use libp2p::{
     mdns, noise, ping,
     request_response::{self, Codec, ProtocolSupport, ResponseChannel},
     swarm::NetworkBehaviour,
     tcp, yamux, Multiaddr, PeerId, Swarm,
 };
+use serde::{Deserialize, Serialize};
+use std::io::{Error, ErrorKind};
 use std::{collections::HashMap, future::Future, pin::Pin, time::Duration};
-use tokio::io::{AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 use tokio::{io, select};
-
 // Define a simple protocol
 #[derive(Debug, Clone)]
 struct MessageProtocol;
 
 // Define request and response types
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct MessageRequest(String);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct MessageResponse(String);
 
 // Implement the codec for our protocol
@@ -33,21 +34,51 @@ impl Codec for MessageCodec {
 
     fn read_request<'life0, 'life1, 'life2, 'async_trait, T>(
         &'life0 mut self,
-        _protocol: &'life1 Self::Protocol,
+        protocol: &'life1 Self::Protocol,
         io: &'life2 mut T,
-    ) -> Pin<Box<dyn Future<Output = Result<Self::Request, std::io::Error>> + Send + 'async_trait>>
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = std::io::Result<Self::Request>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
     where
-        T: tokio::io::AsyncRead + Unpin + Send + 'async_trait,
+        T: futures::AsyncRead + Unpin + Send,
+        T: 'async_trait,
         'life0: 'async_trait,
         'life1: 'async_trait,
         'life2: 'async_trait,
         Self: 'async_trait,
     {
         Box::pin(async move {
-            let mut buf = Vec::new();
+            let mut buffer = vec![0u8; 1024];
+            let mut total_size = 0;
 
-            io.read_to_end(&mut buf).await?;
-            Ok(MessageRequest(String::from_utf8_lossy(&buf).into_owned()))
+            loop {
+                let poll_result = poll_fn(|cx| {
+                    // Borrow `io` explicitly and ensure it is pinned
+                    Pin::new(&mut *io).poll_read(cx, &mut buffer[total_size..])
+                })
+                .await;
+
+                match poll_result {
+                    Ok(size) => {
+                        if size == 0 {
+                            return Err(Error::new(ErrorKind::UnexpectedEof, "Connection closed"));
+                        }
+                        total_size += size;
+
+                        // Attempt to deserialize
+                        if let Ok(request) =
+                            bincode::deserialize::<MessageRequest>(&buffer[..total_size])
+                        {
+                            return Ok(request);
+                        }
+                    }
+                    Err(e) => return Err(e),
+                }
+            }
         })
     }
 }
@@ -55,7 +86,6 @@ impl Codec for MessageCodec {
 #[derive(NetworkBehaviour)]
 struct Behaviour {
     mdns: mdns::tokio::Behaviour,
-    ping: ping::Behaviour,
     request_response: request_response::Behaviour<MessageCodec>,
 }
 
@@ -69,61 +99,7 @@ async fn try_dial_peer(swarm: &mut Swarm<Behaviour>, peer_address: Multiaddr) {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
-        .with_tokio()
-        .with_tcp(
-            tcp::Config::default(),
-            noise::Config::new,
-            yamux::Config::default,
-        )?
-        .with_behaviour(|key| {
-            let mdns =
-                mdns::tokio::Behaviour::new(mdns::Config::default(), key.public().to_peer_id())?;
+    Ok(())
 
-            let ping = ping::Behaviour::default();
-
-            let request_response = request_response::Behaviour::new(protocols, cfg);
-            Ok(Behaviour {
-                mdns,
-                ping,
-                request_response,
-            })
-        })?
-        .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(60)))
-        .build();
-
-    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
-
-    let mut peer_streams: HashMap<PeerId, (Multiaddr, tokio::io::DuplexStream)> = HashMap::new();
-
-    loop {
-        select! {
-            event = swarm.select_next_some() => match event {
-                SwarmEvent::Behaviour(BehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
-                    for (peer_id, peer_address) in list {
-                        println!("mDNS discovered a new peer: {peer_id}");
-
-                        try_dial_peer(&mut swarm, peer_address.clone()).await;
-
-
-                        let _ = swarm.select_next_some().await;
-                        //TODO:
-
-                    }
-                },
-                SwarmEvent::Behaviour(BehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
-                    for (peer_id, _multiaddr) in list {
-                        println!("mDNS discover peer has expired: {peer_id}");
-                    }
-                },
-
-
-                SwarmEvent::NewListenAddr { address, .. } => {
-                    println!("Local node is listening on {address}");
-                }
-                _ => {}
-            }
-        }
-    }
     // You need to await the startup function to execute the future it returns
 }
